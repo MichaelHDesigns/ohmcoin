c974c06e982621abf5742234f7aa8c98eabbbf65
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-01-30 23:00:03 -0500

    Update net.cpp

--- src/rpc/net.cpp
+++ src/rpc/net.cpp
@@ -174,7 +174,7 @@ UniValue addnode(const UniValue& params, bool fHelp)
             "1. \"node\"     (string, required) The node (see getpeerinfo for nodes)\n"
             "2. \"command\"  (string, required) 'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once\n"
             "\nExamples:\n" +
-            HelpExampleCli("addnode", "\"192.168.0.6:52020\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:52020\", \"onetry\""));
+            HelpExampleCli("addnode", "\"192.168.0.6:88888\" \"onetry\"") + HelpExampleRpc("addnode", "\"192.168.0.6:88888\", \"onetry\""));
 
     string strNode = params[0].get_str();
 
@@ -244,7 +244,7 @@ UniValue getaddednodeinfo(const UniValue& params, bool fHelp)
             "    \"connected\" : true|false,          (boolean) If connected\n"
             "    \"addresses\" : [\n"
             "       {\n"
-            "         \"address\" : \"192.168.0.201:52020\",  (string) The ohmcoin server host and port\n"
+            "         \"address\" : \"192.168.0.201:88888\",  (string) The ohmcoin server host and port\n"
             "         \"connected\" : \"outbound\"           (string) connection, inbound or outbound\n"
             "       }\n"
             "       ,...\n"
